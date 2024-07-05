930e4f4b432b9d94f859c8767ceb7c40d635a5cb
Arina <arina.milyutina.0005@gmail.com>
2024-07-05 22:16:43 +0300

    create interface with methods for working with input and output

---
+++ src/main/java/ru/clevertec/check/console/util/Reader.java
@@ -0,0 +1,7 @@
+package main.java.ru.clevertec.check.console.util;
+
+public interface Reader {
+    Double readDouble();
+    String readString();
+    int readInt();
+}
---
+++ src/main/java/ru/clevertec/check/console/util/Writer.java
@@ -0,0 +1,5 @@
+package main.java.ru.clevertec.check.console.util;
+
+public interface Writer {
+     void write(String msg);
+}
