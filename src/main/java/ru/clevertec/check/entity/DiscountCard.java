afcc2ffb6b81ac0769cb41df20a62827b1e337f2
Arina <arina.milyutina.0005@gmail.com>
2024-07-05 23:07:38 +0300

    add getter and override method toString

--- src/main/java/ru/clevertec/check/entity/DiscountCard.java
+++ src/main/java/ru/clevertec/check/entity/DiscountCard.java
@@ -4,4 +4,26 @@ public class DiscountCard {
     private long id;
     private int number;
     private int discountAmount;
+
+    public long getId() {
+        return id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "DiscountCard{" +
+                "id=" + id +
+                ", number=" + number +
+                ", discountAmount=" + discountAmount +
+                '}';
+    }
+
 }
--- src/main/java/ru/clevertec/check/entity/Product.java
+++ src/main/java/ru/clevertec/check/entity/Product.java
@@ -5,4 +5,30 @@ public class Product {
     private double price;
     private int quantityInStock;
     private WholesaleProduct wholesaleProduct;
+
+    public long getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public WholesaleProduct getWholesaleProduct() {
+        return wholesaleProduct;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", price=" + price +
+                ", quantityInStock=" + quantityInStock +
+                ", wholesaleProduct=" + wholesaleProduct +
+                '}';
+    }
 }
