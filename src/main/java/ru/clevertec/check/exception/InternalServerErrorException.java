c667e63df258c32f221bee45174ce150dee7b512
Arina <arina.milyutina.0005@gmail.com>
2024-07-10 23:47:14 +0300

    created classes for working with exception

---
+++ src/main/java/ru/clevertec/check/exception/InternalServerErrorException.java
@@ -0,0 +1,7 @@
+package main.java.ru.clevertec.check.exception;
+
+public class InternalServerErrorException extends Exception{
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+}
