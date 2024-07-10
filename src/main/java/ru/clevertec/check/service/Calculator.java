e74d4697014108fa6c6424c13c962aba226fca1f
Arina <arina.milyutina.0005@gmail.com>
2024-07-10 20:47:23 +0300

    created interface Calculator with methods that count the check's indicators

--- src/main/java/ru/clevertec/check/service/Calculator.java
+++ src/main/java/ru/clevertec/check/service/Calculator.java
@@ -1,5 +1,14 @@
 package main.java.ru.clevertec.check.service;
 
+import main.java.ru.clevertec.check.entity.Product;
+
+import java.util.List;
+
 public interface Calculator {
+    double calculateTotalPrice(List<Product> productsList, String input);
+
+    double calculateTotal(int count, double price);
+
+    double calculateDiscount(int discountAmount, double total);
 
 }
