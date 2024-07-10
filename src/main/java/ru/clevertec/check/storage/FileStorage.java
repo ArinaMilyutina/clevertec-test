759ada686c06234aa82fc3bde749f5fd05116bbd
Arina <arina.milyutina.0005@gmail.com>
2024-07-10 20:44:13 +0300

    added Stream Api and lambda expression in readProductsFromCSV, created method for reading card by number

--- src/main/java/ru/clevertec/check/storage/FileStorage.java
+++ src/main/java/ru/clevertec/check/storage/FileStorage.java
@@ -5,16 +5,18 @@ import main.java.ru.clevertec.check.entity.Product;
 import main.java.ru.clevertec.check.entity.WholesaleProduct;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileStorage {
     private static final String FILENAME = "src/result.csv";
+    private static final String FILENAME_CARD = "./src/main/resources/discountCards.csv";
     private static final String SPLITTER_CSV = ";";
     private static final String WHOLESALE = "+";
     private static final String POINT = ".";
     private static final String COMMA = ",";
-    private static final ConsoleWriter writer=new ConsoleWriter();
+    private static final int DEFAULT_DISCOUNT = 2;
+    private static final ConsoleWriter writer = new ConsoleWriter();
 
 
     public void createFile() {
@@ -33,37 +35,43 @@ public class FileStorage {
     }
 
     public List<Product> readProductsFromCSV(String filename) {
-        List<Product> productsList = new ArrayList<>();
-
+        List<Product> productsList;
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            boolean firstLine = true;
-            while ((line = br.readLine()) != null) {
-                if (firstLine) {
-                    firstLine = false;
-                    continue;
-                }
-                String[] data = line.trim().split(SPLITTER_CSV);
-
-                if (data.length >= 5) {
-                    int id = Integer.parseInt(data[0].trim());
-                    double price = Double.parseDouble(data[2].trim().replace(COMMA, POINT));
-                    int quantityInStock = Integer.parseInt(data[3].trim());
-                    WholesaleProduct wholesaleProduct = data[4].trim().equals(WHOLESALE) ? WholesaleProduct.WHOLESALE : WholesaleProduct.REGULAR;
-
-                    String description;
-                    if (data.length > 5) {
-                        description = data[1].trim() + SPLITTER_CSV + data[5].trim();
-                    } else {
-                        description = data[1].trim();
-                    }
-                    Product product = new Product(id, price, quantityInStock, wholesaleProduct, description);
-                    productsList.add(product);
-                }
-            }
+            productsList = br.lines()
+                    .skip(1)
+                    .map(line -> line.trim().split(SPLITTER_CSV))
+                    .filter(data -> data.length >= 5)
+                    .map(data -> {
+                        int id = Integer.parseInt(data[0].trim());
+                        double price = Double.parseDouble(data[2].trim().replace(COMMA, POINT));
+                        int quantityInStock = Integer.parseInt(data[3].trim());
+                        WholesaleProduct wholesaleProduct = data[4].trim().equals(WHOLESALE) ? WholesaleProduct.WHOLESALE : WholesaleProduct.REGULAR;
+                        String description = data.length > 5 ? data[1].trim() + SPLITTER_CSV + data[5].trim() : data[1].trim();
+                        return new Product(id, price, quantityInStock, wholesaleProduct, description);
+                    })
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return productsList;
     }
+
+    public int readDiscountCardsFromCSVByNumber(String cardNumber) {
+        String line = "";
+        int discount = DEFAULT_DISCOUNT;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME_CARD))) {
+            while ((line = br.readLine()) != null) {
+                String[] card = line.split(SPLITTER_CSV);
+                if (card[1].equals(cardNumber)) {
+                    discount = Integer.parseInt(card[2]);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return discount;
+    }
 }
+
