194c241ddee291f55934c4257104db668128553c
Arina <arina.milyutina.0005@gmail.com>
2024-07-10 22:53:06 +0300

    corrected methods for creation check

--- src/main/java/ru/clevertec/check/service/CheckService.java
+++ src/main/java/ru/clevertec/check/service/CheckService.java
@@ -2,6 +2,7 @@ package main.java.ru.clevertec.check.service;
 
 import main.java.ru.clevertec.check.console.util.ConsoleWriter;
 import main.java.ru.clevertec.check.entity.Check;
+import main.java.ru.clevertec.check.entity.DiscountCard;
 import main.java.ru.clevertec.check.entity.Product;
 import main.java.ru.clevertec.check.entity.Products;
 
@@ -16,7 +17,8 @@ public class CheckService {
     private static final ConsoleWriter writer = new ConsoleWriter();
     private static final String SPACING = " ";
     private static final ProductService productService = new ProductService();
-    private static final CalculatorService calculatorService = new CalculatorService();
+    private static final CalculatorCheckService calculatorService = new CalculatorCheckService();
+    private static final DiscountCardService cardService = new DiscountCardService();
 
     public void printCheck(Check check) {
         writer.write("Check Date: " + check.getDate());
@@ -24,27 +26,30 @@ public class CheckService {
 
         for (Products products : check.getProductList()) {
             Product product = products.getProduct();
-            int count = products.getCount();
-            writer.write("Product ID: " + product.getId() + ", Price: " + product.getPrice() + ", Quantity: " + count + ", Description: " + product.getDescription());
+            writer.write("Price: " + product.getPrice() + ", Quantity: " + products.getCount() + ", Description: " + product.getDescription() + ", Total: " + products.getTotal() + ", Discount: " + products.getDiscount());
         }
+        writer.write("Discount card: " + check.getDiscountCard().getNumber());
+        writer.write("Discount percentage: " + check.getDiscountCard().getDiscountAmount());
 
         writer.write("Total Price: " + check.getTotalPrice());
+        writer.write("Total Discount: " + check.getTotalDiscount());
+        writer.write("Total With Discount: " + check.getTotalPriceWithDiscount());
     }
 
-    public Check createCheck(List<Product> productsList, String input) {
+    public Check createCheck(List<Product> productsList, String input, int discount, String number) {
+        Map<Product, Integer> chosenProductsMap = new HashMap<>();
         double totalPrice = calculatorService.calculateTotalPrice(productsList, input);
 
-        Map<Product, Integer> chosenProductsMap = new HashMap<>();
         String[] inputArr = input.split(SPACING);
-
         for (String item : inputArr) {
             productService.processInputItem(productsList, item, chosenProductsMap, totalPrice);
         }
-
         List<Products> chosenProducts = new ArrayList<>();
-        productService.createChosenProductsList(chosenProductsMap, chosenProducts);
-
-        return new Check(LocalDate.now(), LocalTime.now(), chosenProducts, totalPrice);
+        productService.createChosenProductsList(chosenProductsMap, chosenProducts, discount);
+        double totalDiscount = calculatorService.calculateTotalDiscount(chosenProducts);
+        double totalWithDiscount = calculatorService.calculateTotalWithDiscount(totalPrice, totalDiscount);
+        DiscountCard discountCard = new DiscountCard(number, cardService.discountAmount(number));
+        return new Check(LocalDate.now(), LocalTime.now(), chosenProducts, totalPrice, discountCard, totalDiscount, totalWithDiscount);
     }
 
 }
