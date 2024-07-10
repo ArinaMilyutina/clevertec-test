3f5d905d3640572674eb91a2c52aa9ec066432f5
Arina <arina.milyutina.0005@gmail.com>
2024-07-10 22:37:30 +0300

    created methods for counting totalDiscount, totalPriceWithDiscount, discountWholesaleProduct

--- src/main/java/ru/clevertec/check/service/Calculator.java
+++ src/main/java/ru/clevertec/check/service/Calculator.java
@@ -1,6 +1,7 @@
 package main.java.ru.clevertec.check.service;
 
 import main.java.ru.clevertec.check.entity.Product;
+import main.java.ru.clevertec.check.entity.Products;
 
 import java.util.List;
 
@@ -10,5 +11,11 @@ public interface Calculator {
     double calculateTotal(int count, double price);
 
     double calculateDiscount(int discountAmount, double total);
+
+    double calculateTotalDiscount(List<Products> chosenProducts);
+
+    double calculateTotalWithDiscount(double totalPrice, double discount);
+
+    double calculateDiscountWholesaleProduct(double total);
 
 }
