39a0cceacc25f40a8f718b376a7ff4578873034a
Arina <arina.milyutina.0005@gmail.com>
2024-07-11 04:11:21 +0300

    add getter and setter

--- src/main/java/ru/clevertec/check/entity/user/DebitCard.java
+++ src/main/java/ru/clevertec/check/entity/user/DebitCard.java
@@ -3,4 +3,12 @@ package main.java.ru.clevertec.check.entity.user;
 public class DebitCard {
     private long id;
     private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
